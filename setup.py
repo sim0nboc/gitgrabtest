80a09c8f37d11ff9eafbdec2e4647a260447e8d7
sim0nboc <drybones.ace@gmail.com>
2019-10-02 11:46:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='gitgrabtest',
     version='0.1dev',
-    packages=['gitgrabtest',],
+    packages=['popups',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
